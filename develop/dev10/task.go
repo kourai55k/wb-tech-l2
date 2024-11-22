9b60f89d0eecb1e6791cd8ab2611cb9addb93b42
kourai.55k <kourai.55k@gmail.com>
2024-11-22 15:51:07 +0300

    dev 10 complete

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,1 +1,51 @@
-package dev10
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	// Разбираем аргументы командной строки
+	timeout := flag.String("timeout", "10s", "Connection timeout duration")
+	flag.Parse()
+
+	host := flag.Args()[0]
+	port := flag.Args()[1]
+
+	// Преобразуем timeout в объект типа time.Duration
+	timeoutDuration, err := time.ParseDuration(*timeout)
+	if err != nil {
+		fmt.Println("Invalid timeout duration:", err)
+		return
+	}
+
+	// Формируем строку для подключения
+	address := fmt.Sprintf("%s:%s", host, port)
+
+	// Устанавливаем TCP соединение с таймаутом
+	conn, err := net.DialTimeout("tcp", address, timeoutDuration)
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Запускаем горутину для чтения данных из сокета и вывода в STDOUT
+	go func() {
+		_, err := io.Copy(os.Stdout, conn)
+		if err != nil {
+			fmt.Println("Error reading from server:", err)
+		}
+	}()
+
+	// Копируем данные из STDIN в сокет
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil {
+		fmt.Println("Error sending data to server:", err)
+	}
+}
